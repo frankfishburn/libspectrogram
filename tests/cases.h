445585c00af2595c084aa9d47fa4acadf8a71857
Frank Fishburn <frankthefishburn@gmail.com>
2018-12-31 19:51:35 -0500

    Refactor unit tests

---
+++ tests/cases.h
@@ -0,0 +1,14 @@
+#ifndef CASES_H
+#define CASES_H
+
+#include "stft_tester.h"
+
+class STFT_Test_1 : public STFT_Tester{void SetUp();};
+class STFT_Test_2 : public STFT_Tester{void SetUp();};
+class STFT_Test_3 : public STFT_Tester{void SetUp();};
+class STFT_Test_4 : public STFT_Tester{void SetUp();};
+class STFT_Test_5 : public STFT_Tester{void SetUp();};
+class STFT_Test_6 : public STFT_Tester{void SetUp();};
+
+#endif /* CASES_H */
+
